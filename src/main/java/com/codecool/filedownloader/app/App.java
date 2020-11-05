e19c680c3053d1a6559bb23dd2df5f05d949864f
bodroghykristof <bodroghykristof@gmail.com>
2020-11-05 22:45:40 +0100

    Create Downloader class and place downloading logic there

--- src/main/java/com/codecool/filedownloader/app/App.java
+++ src/main/java/com/codecool/filedownloader/app/App.java
@@ -1,22 +1,16 @@
 package com.codecool.filedownloader.app;
 
-import java.io.FileOutputStream;
+import com.codecool.filedownloader.network.Downloader;
+
 import java.io.IOException;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
+
 
 public class App {
 
     public static void main(String[] args) throws IOException {
 
-        URL url = new URL("https://www.bbc.com/");
-        ReadableByteChannel readableByteChannel = Channels.newChannel(url.openStream());
-        FileOutputStream fileOutputStream = new FileOutputStream("src/main/outputs/bbc.html");
-        FileChannel fileChannel = fileOutputStream.getChannel();
-        fileChannel.transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
-
+        Downloader downloader = new Downloader("https://www.bbc.com/", "src/main/outputs/bbc.html");
+        downloader.download();
 
     }
 
---
+++ src/main/java/com/codecool/filedownloader/network/Downloader.java
@@ -0,0 +1,25 @@
+package com.codecool.filedownloader.network;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+
+public class Downloader {
+
+    private final URL url;
+    private final ReadableByteChannel readableByteChannel;
+    FileChannel fileChannel;
+
+    public Downloader(String urlString, String filePath) throws IOException {
+        this.url = new URL(urlString);
+        this.readableByteChannel = Channels.newChannel(url.openStream());
+        this.fileChannel = new FileOutputStream(filePath).getChannel();
+    }
+
+    public void download() throws IOException {
+        fileChannel.transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+    }
+}
