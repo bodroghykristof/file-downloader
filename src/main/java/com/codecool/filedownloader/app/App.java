95d7bf0b1a352a14fee0aac00e660a0200d00e5d
bodroghykristof <bodroghykristof@gmail.com>
2020-11-05 22:47:29 +0100

    Add time logging

--- src/main/java/com/codecool/filedownloader/app/App.java
+++ src/main/java/com/codecool/filedownloader/app/App.java
@@ -9,8 +9,11 @@ public class App {
 
     public static void main(String[] args) throws IOException {
 
+        long start = System.currentTimeMillis();
         Downloader downloader = new Downloader("https://www.bbc.com/", "src/main/outputs/bbc.html");
         downloader.download();
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken: " + (end - start) + " ms");
 
     }
 
