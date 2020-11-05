144abcfe7d95176313cf894c508d223cbef629a6
bodroghykristof <bodroghykristof@gmail.com>
2020-11-05 22:51:49 +0100

    Add 3 different news pages

--- src/main/java/com/codecool/filedownloader/app/App.java
+++ src/main/java/com/codecool/filedownloader/app/App.java
@@ -10,10 +10,18 @@ public class App {
     public static void main(String[] args) throws IOException {
 
         long start = System.currentTimeMillis();
-        Downloader downloader = new Downloader("https://www.bbc.com/", "src/main/outputs/bbc.html");
-        downloader.download();
-        long end = System.currentTimeMillis();
-        System.out.println("Time taken: " + (end - start) + " ms");
+        Downloader downloaderOne = new Downloader("https://www.bbc.com/", "src/main/outputs/bbc.html");
+        downloaderOne.download();
+        System.out.println("BBC done");
+        System.out.println("Time: " + (System.currentTimeMillis() - start) + " ms");
+        Downloader downloaderTwo = new Downloader("https://index.hu/", "src/main/outputs/index.html");
+        downloaderTwo.download();
+        System.out.println("Index done");
+        System.out.println("Time: " + (System.currentTimeMillis() - start) + " ms");
+        Downloader downloaderThree = new Downloader("https://www.telegraph.co.uk/", "src/main/outputs/telegraph.html");
+        downloaderThree.download();
+        System.out.println("Telegraph done");
+        System.out.println("Overall: " + (System.currentTimeMillis() - start) + " ms");
 
     }
 
